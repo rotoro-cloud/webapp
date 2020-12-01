ef480ad46a62c9231ad0053581cef6f27f180791
rotoro <rotoro@rotoro.cloud>
2020-12-01 17:57:56 +0300

    add hello web-app on 5000 port

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+import random
+import signal
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello!"
+
+@app.route('/hello there')
+def hello():
+    phrase = random.choice(["Good day","Glad to see you","Hello"])
+    return phrase + ', my friend.'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
